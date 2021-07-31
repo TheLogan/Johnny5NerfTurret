4e7318ee96fdf9303facc2325695a00fba5acc2c
TheLogan <tl@chabber.com>
2021-07-31 16:40:07 +0200

    Added server

--- BoardController.ts
+++ BoardController.ts
@@ -1,30 +1,31 @@
 import { Board, ESC, Sensor, Servo } from "johnny-five";
-const board = new Board();
-
-/*
-* J5 needs to do the following
-* Rotate axes for aiming
-* Spin up BLDC's for firing
-* Feed ammo to firing mechanism
-*/
-
-board.on("ready", () => {
-  const servoX = new Servo({
-    controller: "PCA9685",
-    range: [0, 180],
-    pin: 0
-  });
-
-  const servoY = new Servo({
-    controller: "PCA9685",
-    range: [0, 180],
-    pin: 1
-  });
-
-  servoX.to(90);
-  servoY.to(90);
-  board.repl.inject({
-    x: servoX,
-    y: servoY
-  })
-});
+export default class boardController {
+  board: Board;
+  servoX: Servo;
+  servoY: Servo;
+  boardReady = false;
+  constructor() {
+    this.board = new Board();
+    this.board.on("ready", () => {
+      this.servoX = new Servo({
+        controller: "PCA9685",
+        range: [0, 180],
+        pin: 0
+       });
+       
+       this.servoY = new Servo({
+         controller: "PCA9685",
+         range: [0, 180],
+         pin: 1
+       });
+       
+      this.boardReady = true;
+     });
+  }
+  
+  moveToPosition(pos: { x: number, y: number }) {
+    if (!this.boardReady) return;
+    this.servoX.to(pos.x)
+    this.servoY.to(pos.y)
+  }
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-// import expres from 'express'
+import express from 'express'
+import BoardController from './BoardController'
+
+const app = express()
+const board = new BoardController()
+
+// {x: angle, y: angle}
+app.post('/move', function (req, res) {
+  let movePos = req.body;
+  if (isNaN(movePos.x) || isNaN(movePos.y)) {
+    res.sendStatus(400);
+    return;
+  }
+
+  board.moveToPosition(movePos);
+})
+
+app.listen(3000)
