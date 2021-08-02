8137dd3c7748eed5e02029ffda4fead3dbfb7392
TheLogan <tl@chabber.com>
2021-08-02 19:31:27 +0200

    Started work on stepper controller

--- StepperController.ts
+++ StepperController.ts
@@ -1,39 +1,58 @@
-const {Board, Stepper} = require("johnny-five");
-const board = new Board();
+import { Board, Stepper, } from "johnny-five";
 
-board.on("ready", () => {
-
-  /**
-   * In order to use the Stepper class, your board must be flashed with
-   * either of the following:
-   *
-   * - AdvancedFirmata https://github.com/soundanalogous/AdvancedFirmata
-   * - ConfigurableFirmata https://github.com/firmata/arduino/releases/tag/v2.6.2
-   *
-   */
-
-  const stepper = new Stepper({
-    type: Stepper.TYPE.DRIVER,
-    stepsPerRev: 200,
-    pins: {
-      step: 12,
-      dir: 11
-    }
-  });
-
-  // Set stepper to 180 RPM, counter-clockwise with acceleration and deceleration
-  stepper.rpm(600).ccw().accel(16000).decel(16000);
+export default class StepperController {
+  board = new Board();
+  stepperX: Stepper | null;
+  isReady = false;
+  CCW = 0;
+  CW = 1;
   
-  // Make 10 full revolutions
-  stepper.step(6000, () => {
+  constructor() {
+    this.board.on("ready", () => {
+      
+      this.stepperX = new Stepper({
+        type: Stepper.TYPE.DRIVER,
+        stepsPerRev: 180,
+        pins: {
+          step: 12,
+          dir: 11,
+        },
+      });
 
-    console.log("Done moving CCW");
+      // Set stepper to 180 RPM, counter-clockwise with acceleration and deceleration
+      // this.stepperX.rpm(720).ccw().accel(87500).decel(87500);
+      // this.stepperX.rpm(180).ccw().step(2000, () => console.log("done"));
+      // this.stepperX.rpm(180).cw().step(2000, () => console.log("done"));
+      this.isReady = true;
+      // Make 10 full revolutions
+      // stepper.step(6000, () => {
+      //   console.log("Done moving CCW");
 
-    // once first movement is done, make 10 revolutions clockwise at previously
-    //      defined speed, accel, and decel by passing an object into stepper.step
-    stepper.step({
-      steps: 6000,
-      direction: Stepper.DIRECTION.CW
-    }, () => console.log("Done moving CW"));
-  });
-});
+      //   // once first movement is done, make 10 revolutions clockwise at previously
+      //   //      defined speed, accel, and decel by passing an object into stepper.step
+      //   stepper.step(
+      //     {
+      //       steps: 6000,
+      //       // @ts-ignore
+      //       direction: Stepper.DIRECTION.CW,
+      //     },
+      //     () => console.log("Done moving CW")
+      //   );
+      // });
+    });
+  }
+
+  moveToPosition(movePos: { x: number, y: number }) {
+    let directionX = movePos.x > 0 ? 0 : 1;
+    let stepX = Math.abs(movePos.x);
+    if (directionX) {
+      this.stepperX.rpm(180).ccw().step(stepX, () => {});
+    } else {
+      this.stepperX.rpm(180).cw().step(stepX, () => {});
+    }
+
+    // this.stepperX.step({ steps: stepX, direction: directionX }, () => {
+    //   console.log("done moving")
+    // })
+  }
+}
